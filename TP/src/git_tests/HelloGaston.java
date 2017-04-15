103aba4fd24c1f87c92bf7d54833694a576790ce
Jay <Jay@DESKTOP-INHA0E7>
2017-04-15 16:55:59 -0300

    Commit proyecto maven desde eclipse

---
+++ TP/src/git_tests/HelloGaston.java
@@ -0,0 +1,15 @@
+package git_tests;
+
+/*Prueba de un helloWorld, creen y ejecuten algún método desde este main 
+ * de alguna clase que haga algo similar a un helloWorld y pusheenlo, 
+ * asi nos aseguramos de que podemos hacerlo sin problemas.*/
+
+public class HelloGaston {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("Hello Kittie git!\n");
+		
+	}
+	
+}
